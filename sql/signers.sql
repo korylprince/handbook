6a73d7aaa798bb3ea63126a10caaf8b2d61aad38
Kory Prince <korylprince@gmail.com>
2016-09-14 14:48:43 -0500

    changed SQL table design

--- sql/signers.sql
+++ sql/signers.sql
@@ -1,13 +1,13 @@
 CREATE TABLE signers (
-    username VARCHAR(255) PRIMARY KEY,
-    employee_id VARCHAR(10),
+    employee_id VARCHAR(10) PRIMARY KEY,
+    username VARCHAR(255),
     firstname VARCHAR(255),
     lastname VARCHAR(255),
     campus VARCHAR(20),
     headers TEXT,
     time DATETIME
 );
-CREATE INDEX signers_employee_id ON signers(employee_id);
+CREATE INDEX signers_username ON signers(username);
 CREATE INDEX signers_firstname ON signers(firstname);
 CREATE INDEX signers_lastname ON signers(lastname);
 CREATE INDEX signers_campus ON signers(campus);
